bfa90f3ccf6183657f58e7e38ef25db1cb31c996
IKedmenec <ivan.kec86@gmail.com>
2024-04-18 17:05:14 +0200

    promjena1

---
+++ pocetno4.js
@@ -0,0 +1,13 @@
+var x = "Lorem ipsum"
+    y = 2,
+    z = "2345",
+    Q = false,
+    w = null;
+
+    x +='3' + 1;
+    var baaa = "b" + "a" + +"a" + "a";
+
+    console.log(baaa);
+
+    console.log(typeof w);
+    
