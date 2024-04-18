cd110e89423a4ad38d0f0aa201971336d4936e0e
IKedmenec <ivan.kec86@gmail.com>
2024-04-18 18:42:55 +0200

    promjena 2a

--- pocetno6.js
+++ pocetno6.js
@@ -1,9 +1,9 @@
 var x = 0;
-
+/// prvo ispisi vrijednos pa onda povecaj varijablu za 1
 console.log(x++);
+/// prvo povecaj vrijednost za 1 pa onda ispisi vrijednost
+console.log(++x); 
 
-console.log(++x);
-
-x = x + 1;
+x = x + 1; // x = x  + 1;
 
 console.log(x);
